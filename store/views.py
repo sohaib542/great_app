d944917598c008b1b5ec6d720b36dd3ceb0804f7
sohaib542 <sohaib542@gmail.com>
2025-05-05 10:41:38 +0500

    changed fouth

--- store/views.py
+++ store/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Product
+from category.models import category
 # Create your views here.
 
-def store(request):
-    products=Product.objects.all().filter(is_active=True)
-    context={'product':products}
+def store(request,category_slug=None):
+    categories=None
+    products=None
+    if category_slug != None:
+        categories=get_object_or_404(category,slug=category_slug)
+        products=Product.objects.filter(category=categories,is_avaliable=True)
+        counts=products.count()
+    else:
+
+        products=Product.objects.all()
+        counts=products.count()
+    context={'product':products,'counts':counts}
     return render(request,'store/store.html',context)
